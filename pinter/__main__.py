ac3aff9feda830fed2ab0a47cf4cf9863b3c8a66
mmarzec <marcinmarzec@gmail.com>
2024-02-10 18:01:15 +0100

    Add printing mac addresses as option

--- pinter/__main__.py
+++ pinter/__main__.py
@@ -3,7 +3,7 @@ import socket
 import sys
 
 
-def collect_intf_addresses(iall=None):
+def collect_intf_addresses(include_all=False):
     interfaces_psutil = psutil.net_if_addrs()
 
     intf_addresses = {}
@@ -12,16 +12,18 @@ def collect_intf_addresses(iall=None):
     intf_names = (intf[1] for intf in socket.if_nameindex())
 
     # filter
-    if not iall:
+    if not include_all:
         intf_names = (intf for intf in intf_names if intf.startswith(('enp', 'eth', 'wl', 'eno', 'br', 'tun')))
         intf_names = (intf for intf in intf_names if not intf.startswith(('br-')))
 
     # get addresses
     for intf in intf_names:
-        intf_addresses[intf] = []
+        intf_addresses[intf] = {'ip': [], 'mac': ''}
         for address in interfaces_psutil[intf]:
             if address.family == socket.AF_INET:
-                intf_addresses[intf].append(address.address)
+                intf_addresses[intf]['ip'].append(address.address)
+            elif address.family == socket.AF_PACKET:
+                intf_addresses[intf]['mac'] = address.address
 
     return intf_addresses
 
@@ -36,6 +38,7 @@ Usage:
 Options:
   a  all interfaces
   h  help
+  m  print mac addresses
 
 ''')
 
@@ -47,22 +50,32 @@ def cli():
     if len(sys.argv) > 1:
         options = sys.argv[1]
 
-    iall = None
+    include_all = None
     if 'a' in options:
-        iall = True
+        include_all = True
 
     if 'h' in options:
         print_help()
 
-    intf_addresses = collect_intf_addresses(iall)
-    intf_col_width = len(max(intf_addresses.keys(), key = len))
+    include_mac = False
+    if 'm' in options:
+        include_mac = True
+
+    intf_addresses = collect_intf_addresses(include_all)
+    intf_col_width = len(max(intf_addresses.keys(), key=len))
+    ip_col_width = len(max([ip for intf in intf_addresses for ip in intf_addresses[intf]['ip']], key=len))
+
     for intf in intf_addresses:
-        if not intf_addresses[intf]:
-            intf_addresses[intf].append('')
+        if not intf_addresses[intf]['ip']:
+            intf_addresses[intf]['ip'].append('')
 
-        # print first ip
-        print(f'{intf:<{intf_col_width}}  \033[36m{intf_addresses[intf][0]}\033[0m')
+        # print first row
+        row = f'{intf:<{intf_col_width}}  \033[36m{intf_addresses[intf]["ip"][0]:<{ip_col_width}}\033[0m'
 
-        # print rest ips
-        for ip_addr in intf_addresses[intf][1:]:
+        if include_mac:
+            row = f'{row}  \033[35m{intf_addresses[intf]["mac"]}\033[0m'
+        print(row)
+
+        # print rest rows
+        for ip_addr in intf_addresses[intf]['ip'][1:]:
             print(f'{"":<{intf_col_width}}  \033[36m{ip_addr}\033[0m')
